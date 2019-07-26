7689be551326c3ea75a37375bed51d8deed423ff
Washington Silva <w-osilva@hotmail.com>
2019-07-26 11:14:18 -0300

    setup rspec as defaut test framework for rails

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module LocawebTweets
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # set rspec as default test framework
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
