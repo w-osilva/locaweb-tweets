5915fcaaf069056caf7123e7dbccebf18ead5a5e
Washington Silva <w-osilva@hotmail.com>
2019-07-30 20:00:08 -0300

    fix sorting and format

--- app/services/twitter/concerns/tweets_format.rb
+++ app/services/twitter/concerns/tweets_format.rb
@@ -5,23 +5,22 @@ module Twitter
       def format_tweet(tweet)
         {
             "screen_name" => tweet["user"]["screen_name"],
-            "profile_link" => "https://twitter.com/#{tweet["user"]["screen_name"]}",
-            "created_at" => tweet["created_at"] ,
-            "favorite_count" => tweet["user"]["favourites_count"],
             "followers_count" => tweet["user"]["followers_count"],
-            "text" => tweet["text"],
-            "link" => "https://twitter.com/#{tweet["user"]["screen_name"]}/status/#{tweet["id_str"]}",
             "retweet_count" => tweet["retweet_count"],
-            "retweeted" => tweet['retweeted']
+            "favorite_count" => tweet["user"]["favourites_count"],
+            "text" => tweet["text"],
+            "created_at" => tweet["created_at"] ,
+            "profile_link" => "https://twitter.com/#{tweet["user"]["screen_name"]}",
+            "link" => "https://twitter.com/#{tweet["user"]["screen_name"]}/status/#{tweet["id_str"]}"
         }
       end
 
       def format_most_relevants(tweets)
-        tweets.map{ |tweet| { tweet["user"]["screen_name"].to_s => [format_tweet(tweet)]} }
+        tweets.map{ |tweet| format_tweet(tweet) }
       end
 
       def format_most_mentions(tweets)
-        tweets.map{ |tweet| format_tweet(tweet) }
+        tweets.map{ |tweet| { tweet["user"]["screen_name"].to_s => [format_tweet(tweet)]} }
       end
 
     end
--- app/services/twitter/concerns/tweets_sort.rb
+++ app/services/twitter/concerns/tweets_sort.rb
@@ -4,13 +4,21 @@ module Twitter
 
       def sort_most_relevants(tweets)
         tweets.sort_by do |tweet|
-          [  -tweet["followers_count"].to_i, -tweet["retweet_count"].to_i, -tweet["favourites_count"].to_i]
+          [
+             -tweet["followers_count"].to_i,
+             -tweet["retweet_count"].to_i,
+             -tweet["favourites_count"].to_i
+          ]
         end
       end
 
       def sort_most_mentions(tweets)
         tweets.sort_by do |tweet|
-          [  -tweet[tweet.first.first][0]["followers_count"].to_i, -tweet[tweet.first.first][0]["retweet_count"].to_i, -tweet[tweet.first.first][0]["favourites_count"].to_i]
+          [
+             -tweet[tweet.keys.first].first["followers_count"].to_i,
+             -tweet[tweet.keys.first].first["retweet_count"].to_i,
+             -tweet[tweet.keys.first].first["favourites_count"].to_i
+          ]
         end
       end
 
